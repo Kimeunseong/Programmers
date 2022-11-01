bb0ff41f97a7ee1f2d391ebcf90338463c442b86
Kimeunseong <eskim1717@gmail.com>
2022-11-01 20:46:34 +0900

    221101

--- Lv2/12_멀리 뛰기.py
+++ Lv2/12_멀리 뛰기.py
@@ -10,14 +10,29 @@
 # 멀리뛰기에 사용될 칸의 수 n이 주어질 때, 효진이가 끝에 도달하는 방법이 몇 가지인지 알아내, 
 # 여기에 1234567를 나눈 나머지를 리턴하는 함수, solution을 완성하세요. 예를 들어 4가 입력된다면, 5를 return하면 됩니다.
 
-def solution(n):
-    cnt1 = 0
-    cnt2 = 0
-    total = 0
+# 1차시도 - 실패(시간초과)
 
-    while cnt1 <= n:
-        # (2*cnt2), (1*cnt1)으로 줄세우는 경우의 수 연산 => total에 저장
+import itertools
+
+def solution(n):
+    cnt1 = n % 2    # 1
+    cnt2 = n // 2   # 2
+    result = 0
+    
+    while cnt2 >= 0:
+        arr = []
+        for i in range(cnt1):
+            arr.append(1)
+        for i in range(cnt2):
+            arr.append(2)
+
+        result += len(set(itertools.permutations(arr, len(arr))))
+
         cnt1 += 2
         cnt2 -= 1
     
-    return total
+    return result
+
+print(solution(3))
+print(solution(4))
+print(solution(5))

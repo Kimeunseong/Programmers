b02e8703747def9f7d7874470b9711a42d1c97a2
Kimeunseong <eskim1717@gmail.com>
2022-11-11 16:10:02 +0900

    221111

--- Lv1/52_[카카오 인턴] 키패드 누르기.py
+++ Lv1/52_[카카오 인턴] 키패드 누르기.py
@@ -4,16 +4,14 @@
 # 각 번호를 누른 엄지손가락이 왼손인 지 오른손인 지를 나타내는 연속된 문자열 형태로 return 하도록 solution 함수를 완성해주세요.
 
 def solution(numbers, hand):
-
     answer = ''
-
     pad = {1:[0,0], 2:[0,1], 3:[0,2],
            4:[1,0], 5:[1,1], 6:[1,2],
            7:[2,0], 8:[2,1], 9:[2,2],
-           '*':[3,0], 0:[3,1], '#':[3,1]}
+           '*':[3,0], 0:[3,1], '#':[3,2]}
 
-    target_left = [3,0]
-    target_right = [3,1]
+    target_left = pad['*']
+    target_right = pad['#']
 
     for i in numbers:
         if i in [1, 4, 7]:
@@ -44,5 +42,3 @@ def solution(numbers, hand):
 print(solution([1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5], "right")) # "LRLLLRLLRRL"
 print(solution([7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2], "left")) # "LRLLRRLLLRR"
 print(solution([1, 2, 3, 4, 5, 6, 7, 8, 9, 0], "right")) # "LLRLLRLLRL"
-
-# => 테스트 2, 8, 12, 17, 18 실패...

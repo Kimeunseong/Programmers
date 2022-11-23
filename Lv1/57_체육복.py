ca213836a4292fa7ded4ba7b8bd9bccafd493ae4
Kimeunseong <eskim1717@gmail.com>
2022-11-23 21:10:49 +0900

    221123

--- Lv1/57_체육복.py
+++ Lv1/57_체육복.py
@@ -10,6 +10,8 @@
 # 여벌의 체육복을 가져온 학생들의 번호가 담긴 배열 reserve가 매개변수로 주어질 때, 
 # 체육수업을 들을 수 있는 학생의 최댓값을 return 하도록 solution 함수를 작성해주세요.
 
+from collections import deque
+
 def solution(n, lost, reserve):
     answer = 0
     return answer
